4888dba06c46de584f6514129932e5500bda7ad6
thegoad2010 <hectoremanuelmamaniaruni@gmail.com>
2026-01-05 17:20:51 -0400

    init_db.py

--- init_db.py
+++ init_db.py
@@ -1,10 +1,1 @@
-from app import app, db
 
-def init_db():
-    with app.app_context():
-        print("Creando tablas de base de datos...")
-        db.create_all()
-        print("Tablas creadas exitosamente.")
-
-if __name__ == "__main__":
-    init_db()
