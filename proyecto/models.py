8252fddd659cf76a4027b6fcf65e7ac7677a6945
thegoad2010 <hectoremanuelmamaniaruni@gmail.com>
2026-01-06 10:08:35 -0400

    Update models.py

--- proyecto/models.py
+++ proyecto/models.py
@@ -1,17 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
-
 db = SQLAlchemy()
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     registration_date = db.Column(db.DateTime, default=datetime.utcnow)
     conversions = db.relationship('Conversion', backref='author', lazy='dynamic')
-
 class Conversion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
